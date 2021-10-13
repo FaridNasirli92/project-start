6fb494b2bc6f4409095ae42d3c9a015d91370f54
FARID NASIRLI <91443050+FaridNasirli92@users.noreply.github.com>
2021-10-13 19:50:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var database = [
+	{
+		username: "Farid",
+		password: "lotu"
+	}	
+];
+
+var newsFeed = [
+	{
+		username: "Bobby",
+		timeline: "I'm tired of learning"
+	},
+
+	{
+		username: "Shapalag",
+		timeline: "I'm tired of working"
+	},
+];
+
+var userNamePrompt = prompt ("What's your username?");
+var passwordPrompt = prompt ("What is your password?");
